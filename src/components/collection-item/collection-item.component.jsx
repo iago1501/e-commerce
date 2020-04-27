2e9b565b8c8929e87ef802603bfe968d881a4fa9
Iago Espinoza <iespinoza632@beijaflore.com>
2020-04-27 18:25:48 -0300

    add to cart state functionality not persists

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButtom from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
+const CollectionItem = ({ item, addItem }) => {
 
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+  const {name, price, imageUrl} = item
+
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButtom onClick={() => addItem(item)} inverted>Add to cart</CustomButtom>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
